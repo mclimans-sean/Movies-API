843c2ac89d85eb4cb74cdc8aedda19fa3516ff7c
SEAN MCLIMANS <seanmclimans@gmail.com>
2018-02-14 10:46:07 -0700

    list all movies and genres

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # MOVIES-API
+
+## An API for Movies and their Genres
+
+## Routes - Movies:
+
+### GET route - List all movies
+
+* /api/v1/movies
+
+### GET route - List one movie
+
+* /api/v1/movies/<movie id>
+* example: /api/v1/movies/10
+* returns:
+* {
+  "id": 10,
+  "title": "GoldenEye (1995)",
+  "genre": [
+  {
+  "id": 1,
+  "name": "Action"
+  },
+  {
+  "id": 2,
+  "name": "Adventure"
+  },
+  {
+  "id": 16,
+  "name": "Thriller"
+  }
+  ]
+  }
+
+### POST route - add a movie
+
+* /api/v1/movies
+* Structure example:
+* {
+  "id": 151,
+  "name": "The Others Guys (2010)"
+  }
+
+### PUT route - Update a movie
+
+* /api/v1/movies/<movie id>
+
+### DELETE route - Delete a movie
+
+* /api/v1/movies/<movie id>
+
+## Routes - Genres:
+
+### GET route - List all Genres
+
+* /api/v1/genres
+
+### GET route - List movies associated with specific genre
+
+* /api/v1/genres/<genre id>
+* example: /api/v1/genres/5
--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,43 @@ const knex = require('./knex');
 
 module.exports = {
   getAllMovies() {
-    return knex('movie');
+    // return knex('movie');
+    function groupGenres(genres) {
+      return genres.reduce((result, genre) => {
+        result[genre.movie_id] = result[genre.movie_id] || [];
+        result[genre.movie_id].push(genre);
+        return result;
+      }, {});
+    }
+
+    function populateMovies(movies, genres) {
+      let groupedGenres = groupGenres(genres);
+
+      return movies.map(movie => {
+        movie.genres = groupedGenres[movie.id] || [];
+        console.log(movie);
+        return movie;
+      });
+    }
+
+    function getGenresForAllMovies(movies) {
+      return knex('genre')
+        .join('movie_genre', 'genre_id', 'genre.id')
+        .whereIn('movie_id', movies.map(movie => movie.id));
+    }
+
+    function getMovies() {
+      return knex('movie');
+    }
+
+    function getMoviesWithGenres() {
+      return getMovies().then(movies => {
+        return Promise.all([getGenresForAllMovies(movies)]).then(results => {
+          populateMovies(movies, ...results);
+        });
+      });
+    }
+    return getMoviesWithGenres();
   },
 
   getOneMovie(id) {
@@ -13,7 +49,7 @@ module.exports = {
     }
     function getGenresForMovies(movieId) {
       return knex('genre')
-        .select('name')
+        .select('genre.id', 'name')
         .join('movie_genre', 'genre_id', 'genre.id')
         .where('movie_id', movieId);
     }
